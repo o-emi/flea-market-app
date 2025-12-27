96a321ac3ab70d1a1d5a483aac17218ab25e1dcf
omine emi <rep.pro.o.e@gmail.com>
2025-12-28 00:45:42 +0900

    プロフィール設定のユーザー情報機能を実装（プロフィール画像を除く）

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -7,8 +7,27 @@ use Illuminate\Http\Request;
 
 class MypageController extends Controller
 {
+    public function index()
+    {
+        return view('mypage.index');
+    }
+
     public function edit()
     {
-        return view('mypage/profile');
+        return view('mypage.profile');
+    }
+
+    public function update(Request $request)
+    {
+        auth()->user()->update(
+            $request->only([
+                'name',
+                'postal_code',
+                'address',
+                'building_name',
+            ])
+        );
+
+      return redirect()->route('items.index');
     }
 }
