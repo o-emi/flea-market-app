bd47e4a9bed3283da3e46640fa275bc1325e46c9
omine emi <rep.pro.o.e@gmail.com>
2025-12-30 00:52:11 +0900

    いいね機能実装

---
+++ src/resources/views/items/detail.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app')
+
+@section('title', '商品詳細画面')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/items/detail.css')}}">
+@endsection
+
+@section('content')
+<div class="item-detail-form">
+  <div class="item-detail-form__inner">
+
+    <div class="item-detail-form__image-wrapper">
+      <img src="{{ asset($item->image_url) }}" alt="{{ $item->name }}" class="item-detail-form__image">
+    </div>
+
+    <div class="item-detail-form__content">
+
+      <section class="item-detail-form__header">
+        <h1 class="item-detail-form__name">{{ $item->name }}</h1>
+        <p class="item-detail-form__brand">{{ $item->brand }}</p>
+
+        <div class="item-detail-form__price-group">
+          <span class="item-detail-form__price">¥{{ number_format($item->price) }}</span>
+          <span class="item-detail-form__tax">(税込)</span>
+        </div>
+
+        <div class="item-detail-form__actions">
+          <div class="item-detail-form__action-item">
+            @auth
+              <form action="{{ route('items.like', $item) }}" method="POST" class="item-detail-form__like-form">
+                @csrf
+                <button type="submit" class="item-detail-form__icon-btn">
+                  <img src="{{ asset(auth()->user()->likes()->where('item_id', $item->id)->exists() ? 'images/logo/heart.logo.pink.png' : 'images/logo/heart.logo.gray.png') }}" class="item-detail-form__icon" alt="いいね">
+                </button>
+              </form>
+            @endauth
+            @guest
+              <img src="{{ asset('images/logo/heart.logo.gray.png') }}" class="item-detail-form__icon" alt="いいね">
+            @endguest
+            <span class="item-detail-form__count">{{ $item->likes()->count() }}</span>
+          </div>
+
+          <div class="item-detail-form__action-item">
+            <img src="{{ asset('images/logo/comment.png') }}" class="item-detail-form__icon" alt="コメント">
+            <span class="item-detail-form__count">1</span>
+          </div>
+        </div>
+
+        <a href="{{ route('items.purchase', $item) }}" class="item-detail-form__btn item-detail-form__btn--primary">
+          購入手続きへ
+        </a>
+      </section>
+
+      <section class="item-detail-form__section">
+        <h2 class="item-detail-form__section-title">商品説明</h2>
+        <div class="item-detail-form__description">
+          {{ $item->description }}
+        </div>
+      </section>
+
+      <section class="item-detail-form__section">
+        <h2 class="item-detail-form__section-title">商品の情報</h2>
+        <table class="item-detail-form__table">
+          <tr>
+            <th class="item-detail-form__table-header">カテゴリー</th>
+            <td class="item-detail-form__table-data">
+              <span class="item-detail-form__badge">洋服</span>
+              <span class="item-detail-form__badge">メンズ</span>
+            </td>
+          </tr>
+          <tr>
+            <th class="item-detail-form__table-header">商品の状態</th>
+            <td class="item-detail-form__table-data">{{ $item->condition }}</td>
+          </tr>
+        </table>
+      </section>
+
+      <section class="item-detail-form__section">
+        <h2 class="item-detail-form__section-title">コメント(1)</h2>
+        <div class="item-detail-form__comment-list">
+          <article class="item-detail-form__comment">
+            <div class="item-detail-form__comment-user">
+              <div class="item-detail-form__comment-avatar"></div>
+              <span class="item-detail-form__comment-username">admin</span>
+            </div>
+            <div class="item-detail-form__comment-body">
+              こちらにコメントが入ります。
+            </div>
+          </article>
+        </div>
+
+        <div class="item-detail-form__comment-post">
+          <h3 class="item-detail-form__sub-title">商品へのコメント</h3>
+          <textarea class="item-detail-form__textarea"></textarea>
+          <button class="item-detail-form__btn item-detail-form__btn--primary">コメントを送信する</button>
+        </div>
+      </section>
+
+    </div> </div> </div>
+@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MypageController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\LikeController;
 
 Route::get('/', [ItemController::class, 'index'])
     ->name('items.index');
@@ -12,12 +13,26 @@ Route::post('/login', [AuthController::class, 'login'])
     ->middleware('guest')
     ->name('login');
 
+Route::get('/item/{item}', [ItemController::class, 'show'])
+  ->name('items.show');
+
 Route::middleware('auth')->group(function () {
-    Route::get('/item/{item_id}', [ItemController::class, 'show'])
-        ->name('items.show');
+  Route::get('/mypage', [MyPageController::class, 'index'])
+    ->name('mypage.index');
 
-    Route::get('/mypage/profile', [MypageController::class, 'edit'])
-        ->name('mypage.profile');
+  Route::get('/mypage/profile', [MypageController::class, 'edit'])
+    ->name('mypage.profile');
 
-    Route::post('/logout', [AuthController::class, 'logout']) ->name('logout');
+  Route::post('/mypage/profile', [MypageController::class, 'update'])
+        ->name('mypage.profile.update');
+
+  Route::get('/sell', [ItemController::class, 'create'])
+    ->name('sell');
+
+  Route::post('/items/{item}/like', [LikeController::class, 'toggle'])
+    ->name('items.like');
+
+  Route::get('/purchase/{item}', [ItemController::class, 'purchase'])
+    ->name('items.purchase');
+
 });
